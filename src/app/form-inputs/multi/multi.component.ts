b92b5362fab4182c64613701de32e72df1fd6050
Brock_cott <brockaban@gmail.com>
2025-02-28 18:12:11 +0100

    Added multi control system to the the form

--- src/app/form-inputs/multi/multi.component.ts
+++ src/app/form-inputs/multi/multi.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-multi',
@@ -11,9 +12,18 @@ export class MultiComponent {
   @Input() formGroupName!:any;
   @Input() controlname!:any;
   @Input() formGroup!:any;
-  control:any;
+  control!:FormControl;
+  direction:number = 0;
+  directionValues:string[] = ["","","",""];
+
+
   ngOnInit(): void {
     this.control = this.formGroup.controls[this.formGroupName].controls[this.controlname]
-
   }
+
+
+  update(){
+    this.control.patchValue(""+(this.directionValues[0] != "" ? this.directionValues[0] : "0px") + " "+(this.directionValues[1] != "" ? this.directionValues[1] : "0px") + " "+(this.directionValues[2] != "" ? this.directionValues[2] : "0px") + " "+(this.directionValues[3] != "" ? this.directionValues[3] : "0px") + " ")
+  }
+
 }
