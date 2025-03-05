530e1213caec1f29703ed93c57c389b50971ca98
Brock_cott <brockaban@gmail.com>
2025-03-05 23:27:33 +0100

    deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StyleService } from './style.service';
 import { styleI } from './interfaces/style';
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from '../../env';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +14,14 @@ import { styleI } from './interfaces/style';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+  
+  // Initialize Firebase
+   app = initializeApp(firebaseConfig);
+   analytics = getAnalytics(this.app);
 }
